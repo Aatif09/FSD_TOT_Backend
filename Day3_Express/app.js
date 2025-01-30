0e20e64d74942378f15a7f593ec4c6f99d0e31a6
Dr. AJ <09.aatif@gmail.com>
2025-01-30 14:45:35 +0530

    30-01

--- Day3_Express/app.js
+++ Day3_Express/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const fspro = require('fs/promises');
 const { myreadfile } = require('./utils.js');
 const app = express();
 port = 2001;
-
+app.use(express.json()); // read the data from body(parse) from postman
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
@@ -13,6 +14,25 @@ app.get('/data', async (req, res) => {
 });
 app.post('/add', (req, res) => {
   res.send({ name: "Aatif Jamshed", id: "2719" });
+});
+//http://www.localhost:2001/products
+app.post('/products', async (req, res) => {
+  //{"title": "chips","price": 18,"company": "Lays"}
+  console.log(req.body)//body -raw 
+  const newproduct = req.body;
+  const data = [newproduct]
+  await fspro.writeFile("./ writedata.json", JSON.stringify(data));
+  res.json({
+    status: "success",
+  })
+});
+app.post('/product', async (req, res) => {
+  //{"title": "chips","price": 18,"company": "Lays"}
+  console.log(req.body)//body -raw 
+  await fspro.writeFile("./ writedataa.json", []);
+  res.json({
+    status: "success",
+  })
 });
 app.listen(port, () => {
   console.log('Server is running on port 2001');
--- Day3_Express/utils.js
+++ Day3_Express/utils.js
@@ -1,13 +1,11 @@
 const fspro = require("fs/promises");
 const myreadfile = async (loc) => {
   try {
-    const data = await fspro.readFile(loc, "utf8");
-    const realData = JSON.parse(data);
+    const textdata = await fspro.readFile(loc, "utf8");
+    const realData = JSON.parse(textdata);
     return realData;
-  }
-  catch (err) {
+  } catch (err) {
     console.log("Error reading file:", err.message);
   }
 }
-
 module.exports = { myreadfile }
